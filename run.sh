b4b22c9ec08fc473dd70664f8ca6fdf506c1f39d
Pavel Marek <scriptingshrimp@protonmail.com>
2025-12-24 21:33:05 +0100

    Update run.sh to select binary based on OS type, supporting both Darwin and X86 environments

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
-#/bin/bash
+#!/bin/bash
 
-./mcphostX86 \
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    BINARY="./mcphostDarwin"
+else
+    BINARY="./mcphostX86"
+fi
+
+$BINARY \
 --config ./mcp.json \
 --model ollama:devstral-small-2:latest
